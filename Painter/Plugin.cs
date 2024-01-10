5a91c3857e7cfa5d313eb50aa4b30de0af6e63dc
Jacob <git@megumin.org>
2024-01-09 16:43:29 -0800

    Cleanup

--- Painter/Plugin.cs
+++ Painter/Plugin.cs
@@ -5,6 +5,9 @@ namespace ExcellentDsp.Painter;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
-    public void Awake() =>
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} loaded");
+    public void Awake()
+        => Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} loaded");
+
+    public void OnDestroy()
+        => Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} unloaded");
 }
