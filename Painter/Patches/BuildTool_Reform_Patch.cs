70af43f103bf731c6c16aae09d2b2862799ca850
Jacob <git@megumin.org>
2024-01-10 15:46:26 -0800

    Add patch

---
+++ Painter/Patches/BuildTool_Reform_Patch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace ExcellentDsp.Painter.Patches;
+
+/// <summary>Patch for <see cref="BuildTool_Reform"/></summary>
+[HarmonyPatch(typeof(BuildTool_Reform))]
+internal static class BuildTool_Reform_Patch
+{
+    /// <summary>Before each update when foundation cursor is active</summary>
+    [HarmonyPrefix, HarmonyPatch(nameof(BuildTool_Reform.ReformAction))]
+    public static void ReformAction_Prefix()
+        => UIRealtimeTip.Popup(nameof(ReformAction_Prefix));
+}
--- Painter/Patches/UIBuildMenu_Patch.cs
+++ Painter/Patches/UIBuildMenu_Patch.cs
@@ -4,10 +4,11 @@ namespace ExcellentDsp.Painter.Patches;
 
 /// <summary>Patch for <see cref="UIBuildMenu"/></summary>
 [HarmonyPatch(typeof(UIBuildMenu))]
-internal static class UIBuildMenuPatch
+internal static class UIBuildMenu_Patch
 {
     /// <summary>Before opening the "Environment Modification (9)" toolbar</summary>
     [HarmonyPrefix, HarmonyPatch(nameof(UIBuildMenu.OnCategoryButtonClick))]
     public static void OnCategoryButtonClick_Prefix()
-        => Shared.Logger?.LogWarning("OnCategoryButtonClick_Prefix");
+        => UIRealtimeTip.Popup(nameof(OnCategoryButtonClick_Prefix));
+
 }
