02bfa5233c5dca6aa8d8f906d0ff850b61427962
Jacob <git@megumin.org>
2024-01-09 16:37:17 -0800

    Add project files.

---
+++ Painter/Plugin.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx;
+
+namespace ExcellentDsp.Painter;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public void Awake() =>
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} loaded");
+}
